98b547f5489c1e22c9d5e32bcc905c07bb4d107e
MGianetti <mateusgianetti1996@gmail.com>
2022-08-03 18:28:17 -0300

    feat(Routes): Implemented Client/Professional/Product/Service routes

---
+++ routes/products-routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const Products = require("../models/Products");
+
+router.post("/new", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+router.post("/delete", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+router.put("/edit", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+router.get("/all", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+module.exports = router;
